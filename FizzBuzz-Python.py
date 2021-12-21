674e5770e73ad51828137b8e0346488855ec8922
Multiv3rse <92734955+Multiv3rse@users.noreply.github.com>
2021-12-21 17:31:02 +0100

    repaired fizzbuzz for python

--- FizzBuzz-Python.py
+++ FizzBuzz-Python.py
@@ -1,10 +1,13 @@
-def fizbuzz2(n):
-  for i in range(1, n+1):
-    if i%15==0:
-    print("fizzbuzz")
-    elif i % 3 == 0:
-    print("fizz")
-    elif i % 5 == 0:
-    print("buzz")
-    else:
-  print(i)
+def fizzbuzz(n):
+    for i in range(n):
+        if i % 15 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+
+fizzbuzz(100)
